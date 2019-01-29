1aaa6690256eaa63bab72fe2165f2fe02d8c7fe9
JF1HNL <1118040@ed.tus.ac.jp>
2019-01-29 20:48:29 +0900

    test

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const button = document.querySelector(".tweet")
 button.addEventListener("click", () => {
   var time = new Date()
   const content = {
-    text: "a",
+    text: "",
     tag: "アカピー時報"
   }
   for (let key in content) {
     content[key] = encodeURIComponent(content[key])
   }
   let tweeturl =
-    "https://twitter.com/intent/tweet?text=${content.text}&hashtags=${content.tag}"
+    "https://twitter.com/intent/tweet?text=" +
+    content.text +
+    "&hashtags=" +
+    content.tag
   window.open(tweeturl)
 })
