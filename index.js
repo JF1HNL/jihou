ff332420f0271747446ff33169f7ab20f24d932c
JF1HNL <1118040@ed.tus.ac.jp>
2019-01-29 20:43:53 +0900

    test

---
+++ index.js
@@ -0,0 +1,14 @@
+const button = document.querySelector("#tweet")
+button.addEventListener("click", () => {
+  var time = new Date()
+  const content = {
+    text: "a",
+    tag: "アカピー時報"
+  }
+  for (let key in content) {
+    content[key] = encodeURIComponent(content[key])
+  }
+  let tweeturl =
+    "https://twitter.com/intent/tweet?text=${content.text}&hashtags=${content.tag}"
+  window.open(tweeturl)
+})
