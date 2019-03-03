204c5857dd887588dd595fa8eb54839fba71dc33
JF1HNL <1118040@ed.tus.ac.jp>
2019-03-03 13:47:26 +0900

    vue

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-const button = document.querySelector(".tweet")
-button.addEventListener("click", () => {
-  var time = new Date()
-  console.log()
-  const content = {
-    text:
-      "アカピーが" +
-      time.getHours() +
+let Field = new Vue({
+  el: "#field",
+  computed: {
+    tweet_text: function(){
+      var time = new Date();
+      return this.name + "が" + time.getHours() +
       "時" +
       time.getMinutes() +
-      "分をお知らせします。",
-    tag: "アカピー時報"
+      "分をお知らせします。"
+    }
+  },
+  data: {
+    name: "アカピー",
+    rename: false
+  },
+  methods: {
+    tweet: function(){
+      const content = {
+        text: this.tweet_text,
+        tag: "アカピー時報,時報"
+      }
+      for (let key in content) {
+        content[key] = encodeURIComponent(content[key])
+      }
+      let tweeturl =
+        "https://twitter.com/intent/tweet?text=" +
+        content.text +
+        "&hashtags=" +
+        content.tag
+      window.open(tweeturl)
+    }
   }
-  for (let key in content) {
-    content[key] = encodeURIComponent(content[key])
-  }
-  let tweeturl =
-    "https://twitter.com/intent/tweet?text=" +
-    content.text +
-    "&hashtags=" +
-    content.tag
-  window.open(tweeturl)
 })
